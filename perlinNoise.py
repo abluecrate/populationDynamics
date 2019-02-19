acd7ac6f81029bbf5d11f90525c7614e65510785
Ethan Walkowiak <ethanwalkowiak@gmail.com>
2019-02-19 15:15:53 -0500

    added circle gradient for island generation

--- perlinNoise.py
+++ perlinNoise.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def getGradient(seed):
     seed and np.random.seed(seed)
     gradient = np.random.rand(512, 512, 2) * 2 - 1
@@ -9,7 +10,7 @@ def getGradient(seed):
 def fade(values):
     return 6*values**5 - 15*values**4 + 10*values**3
 
-def perlin_noise(sizeX, sizeY, frequency,seed = None):
+def perlin_noise(sizeX, sizeY, frequency, seed = None):
     gradient = getGradient(seed)
     
     x = np.tile(np.linspace(0, frequency, sizeX, endpoint = False), sizeY)
@@ -41,18 +42,51 @@ def perlin_noise(sizeX, sizeY, frequency,seed = None):
 
     g = g0 + fadeY * (g1 - g0)
 
-    return g
+    max = np.amax(g)
+    min = np.amin(g)
+
+    return (g - min) / (max - min)
+
+def calculateCircleGradient(mapArray):
+    sizeX, sizeY = mapArray.shape
+    x = np.tile(np.linspace(0, sizeX, sizeX, endpoint = False), sizeY)
+    x = x.reshape(sizeX, sizeY)
+    y = np.repeat( np.linspace(0, sizeY, sizeY, endpoint = False), sizeX)
+    y = y.reshape(sizeX, sizeY)
+
+    centerPointX = sizeX/2
+    centerPointY = sizeY/2
+
+    distanceFromCenter = abs(np.sqrt((x - centerPointX)**2+ (y - centerPointY)**2) - np.mean([sizeX,sizeY]))
+
+    max = np.amax(distanceFromCenter)
+    min = np.amin(distanceFromCenter)
+
+    # oGrid = np.ogrid[0:mapArray.shape[0], 0:mapArray.shape[1]]
+    # centerPoint = np.array(centerPoint).astype(int)
+    # distanceFromCenter = np.sqrt(((np.argwhere(mapArray > 0) - centerPoint)**2).sum(1)).mean()
+    # distanceFromCenter = cdist(mapArray, np.atleast_2d(centerPoint)).ravel()
+    # distanceFromCenter = np.sqrt(((oGrid - centerPoint)**2).sum(1)).mean()
+
+    return (distanceFromCenter - min) / (max - min)
 
 print('-----------------------------------------------------------------')
 
 seed = None
-map = perlin_noise(64, 64, 4, seed)
 
-print(map.shape)
+noiseMap = perlin_noise(128, 128, 4, seed)
+
+# moistureMap = perlin_noise(64, 64, 2)
+
+circleGradient = calculateCircleGradient(noiseMap)
+
+map = noiseMap * circleGradient
 
 print('-----------------------------------------------------------------')
 
-fig, ax = plt.subplots()
-ax.matshow(map, cmap = plt.cm.Blues)
+fig, (ax1, ax2, ax3) = plt.subplots(1,3)
+ax1.matshow(noiseMap, cmap = plt.cm.Greys)
+ax2.matshow(circleGradient, cmap = plt.cm.Greys)
+ax3.matshow(map, cmap = plt.cm.terrain)
 plt.show()
 
